61868765eb1e887bd9421aba28cc34165620d7c3
Tom Planche <tomplanche@icloud.com>
2023-11-02 00:32:18 +0100

    Initial commit

---
+++ src/bin/year_2015/day_01.rs
@@ -0,0 +1,18 @@
+///
+/// # day_01.rs
+/// Code for the day 01 of the Advent of Code challenge year 2015
+///
+
+// Imports  ==============================================================================  Imports
+
+// Variables  =========================================================================== Variables
+
+// Functions  =========================================================================== Functions
+pub fn response_part_1() {
+	println!("Day 01 - Part 1");
+}
+
+pub fn response_part_2() {
+	println!("Day 01 - Part 2");
+}
+
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+///
+/// # src/main.rs
+/// Contains the main function for the program.
+///
+
+// Imports  ==============================================================================  Imports
+#[path = "./bin/year_2015/day_01.rs"]
+mod day_01;
+
+use day_01::response_part_1;
+// use day_01::response_part_2;
+// Variables  =========================================================================== Variables
+
+// Functions  =========================================================================== Functions
+
+// Main  ====================================================================================  Main
+fn main() {
+	response_part_1();
+	// response_part_2();
+}
