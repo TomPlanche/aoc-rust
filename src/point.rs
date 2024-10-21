5a8cff5eb4ef1aaf715092f98d916469bb7d8ca0
Tom Planche <Tomplanche@icloud.com>
2024-10-21 19:05:41 +0200

    [25] (feat on master) Implemented the `Point` trait for only numbers.

--- src/point.rs
+++ src/point.rs
@@ -1,21 +1,41 @@
 use std::fmt;
 use std::ops::{Add, Sub};
 
+// Define a trait that combines the necessary numeric traits
+pub trait Number:
+    Copy + PartialOrd + Add<Output = Self> + Sub<Output = Self> + fmt::Display
+{
+}
+
+// Implement the Number trait for the built-in numeric types
+impl Number for i8 {}
+impl Number for i16 {}
+impl Number for i32 {}
+impl Number for i64 {}
+impl Number for i128 {}
+impl Number for u8 {}
+impl Number for u16 {}
+impl Number for u32 {}
+impl Number for u64 {}
+impl Number for u128 {}
+impl Number for f32 {}
+impl Number for f64 {}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Point<T> {
+pub struct Point<T: Number> {
     pub x: T,
     pub y: T,
 }
 
 // Most of the solutions will implement their own `FromStr` trait for the `Point` struct.
 #[allow(dead_code)]
-impl<T> Point<T> {
+impl<T: Number> Point<T> {
     pub fn new(x: T, y: T) -> Self {
         Point { x, y }
     }
 }
 
-impl<T: Add<Output = T>> Add for Point<T> {
+impl<T: Number> Add for Point<T> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self::Output {
@@ -26,7 +46,7 @@ impl<T: Add<Output = T>> Add for Point<T> {
     }
 }
 
-impl<T: Sub<Output = T>> Sub for Point<T> {
+impl<T: Number> Sub for Point<T> {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self::Output {
@@ -37,14 +57,14 @@ impl<T: Sub<Output = T>> Sub for Point<T> {
     }
 }
 
-impl<T: fmt::Display> fmt::Display for Point<T> {
+impl<T: Number> fmt::Display for Point<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
 
 #[allow(dead_code)]
-impl<T: Add<Output = T> + Sub<Output = T> + Copy + std::cmp::PartialOrd> Point<T> {
+impl<T: Number> Point<T> {
     pub fn manhattan_distance(&self, other: &Self) -> T {
         let dx = if self.x > other.x {
             self.x - other.x
